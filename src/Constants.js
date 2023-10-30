dffe0392d46df899e528889cb068c29332e40f35
leduyantran <trana19@spu.edu>
2023-10-30 11:23:08 -0700

    Setting up environment

---
+++ src/Constants.js
@@ -0,0 +1,4 @@
+export const API = {
+    key: '',
+    domain: '',
+}
---
+++ src/Pages/GeneralPage/GeneralPage.js
@@ -0,0 +1,15 @@
+// Import Components
+import React from 'react'
+
+//Import Stylings
+import './GeneralPage.css'
+
+//Import Icons
+
+function GeneralPage() {
+  return (
+    <div>GeneralPage</div>
+  )
+}
+
+export default GeneralPage
