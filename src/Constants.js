4cff4a8852eb765917ac96dc01949f7781e831b3
leduyantran <trana19@spu.edu>
2023-10-30 18:04:39 -0700

    configure api constants for production .env

--- src/Constants.js
+++ src/Constants.js
@@ -1,6 +1,6 @@
 const isProduction = process.env.NODE_ENV === 'production';
 
 export const API = {
-    key: isProduction ? process.env.API_KEY : process.env.REACT_APP_API_KEY,
-    domain: isProduction ? process.env.API_DOMAIN : process.env.REACT_APP_API_DOMAIN,
+    key: process.env.REACT_APP_API_KEY,
+    domain: process.env.REACT_APP_API_DOMAIN,
 }
