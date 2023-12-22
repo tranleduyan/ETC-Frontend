bf65c97eb3904cd200a9338fbd931966a7fc837d
Le Duy An Tran <114903308+tranleduyan@users.noreply.github.com>
2023-12-21 23:29:02 -0800

    test prod bug

--- src/Components/Logo/Logo.js
+++ src/Components/Logo/Logo.js
@@ -8,7 +8,7 @@ import './Logo.css';
 function Logo(props) {
   const { className } = props;
   return (
-    <div className={`${className} Logo-Container`}>
+    <div className={`Logo-Container ${className} `}>
       <img src={ETC_Color_Logo} alt='ETC Logo'/>
     </div>
   )
